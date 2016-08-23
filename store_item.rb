9ac81ba852424a6de9d70aabf011bb1ce224a713
risatino <charissatolentino@gmail.com>
2016-08-23 15:55:16 -0500

    error in line 3, initialize has the wrong number of arguments?

--- store_item.rb
+++ store_item.rb
@@ -7,16 +7,32 @@ class Product
     @type = type
   end
 
+  def name=(text)
+    @name = text
+  end
+
   def name
     @name
+  end
+
+  def color=(text)
+    @color = text
   end
 
   def color
     @color
   end
 
+  def price=(number)
+    @price = number
+  end
+
   def price
     @price
+  end
+
+  def type=(text)
+    @type = text
   end
 
   def type
@@ -24,16 +40,18 @@ class Product
   end
 
   def print_item
-    puts "The #{name} are #{color}, are $#{price} and a type of #{type}"
+    puts "The #{name} are #{color} are $#{price} and a type of #{type}"
   end
 
 end    
 
 item_1 = Product.new({name: 'bananas', color: 'yellow', price: 2, type: 'food'})
 item_2 = Product.new({name: 'records', color: 'black', price: 15, type: 'music'})
-item_3 = Product.new({name: 'cars', :color => 'red', price: 55000, type: 'vehicle'})
+item_3 = Product.new({:name => 'cars', :color => 'red', :price => 55000, :type => 'transportation vehicle'})
 
-item_1.print_item
-item_2.print_item
-item_3.print_item
+puts item_1.print_item
+puts item_2.print_item
+puts item_3.print_item
+
+# puts "The " + name + "are " + color + "are " + "$" + price + "and a type of " + type"
 
